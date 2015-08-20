ff2bcfb5c8916d446e3bca324501b2d09f52ad0f
Bryan Boreham <bjboreham@gmail.com>
2015-08-20 10:59:32 +0100

    Leave container allocations for a time after death so they will be reclaimed if the container is restarted

--- test/640_proxy_restart_reattaches_test.sh
+++ test/640_proxy_restart_reattaches_test.sh
@@ -2,17 +2,16 @@
 
 . ./config.sh
 
-C1=10.2.0.78
-C2=10.2.0.34
 NAME=seetwo.weave.local
 
 start_suite "Proxy restart reattaches networking to containers"
 
 weave_on $HOST1 launch
-proxy_start_container          $HOST1 -e WEAVE_CIDR=$C2/24 -dt --name=c2 -h $NAME
-proxy_start_container_with_dns $HOST1 -e WEAVE_CIDR=$C1/24 -dt --name=c1
+proxy_start_container          $HOST1 --name=c2 -h $NAME
+proxy_start_container_with_dns $HOST1 --name=c1
 
-proxy docker_on $HOST1 restart c2
+C2=$(container_ip $HOST1 c2)
+proxy docker_on $HOST1 restart --time=1 c2
 assert_raises "proxy exec_on $HOST1 c2 $CHECK_ETHWE_UP"
 assert_dns_record $HOST1 c1 $NAME $C2
 
