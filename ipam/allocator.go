d1eb7600bd08aade65cfc849e548095a6ee29d96
Bryan Boreham <bjboreham@gmail.com>
2015-09-01 11:09:43 +0100

    Extend the timeout for dead containers to 30 seconds, but remove them if they are destroyed, to reduce starvation when users are creating and destroying containers quickly.

--- ipam/allocator.go
+++ ipam/allocator.go
@@ -23,7 +23,7 @@ const (
 
 	tickInterval         = time.Second * 5
 	MinSubnetSize        = 4 // first and last addresses are excluded, so 2 would be too small
-	containerDiedTimeout = time.Second * 5
+	containerDiedTimeout = time.Second * 30
 )
 
 // operation represents something which Allocator wants to do, but
@@ -231,6 +231,17 @@ func (alloc *Allocator) ContainerDied(ident string) {
 		if _, found := alloc.lookupOwned(ident, alloc.universe); found {
 			alloc.debugln("Container", ident, "died; noting to remove later")
 			alloc.dead[ident] = alloc.now()
+		}
+	}
+}
+
+// ContainerDestroyed called from the updater interface.  Async.
+func (alloc *Allocator) ContainerDestroyed(ident string) {
+	alloc.actionChan <- func() {
+		if _, found := alloc.lookupOwned(ident, alloc.universe); found {
+			alloc.debugln("Container", ident, "destroyed; removing addresses")
+			alloc.delete(ident)
+			delete(alloc.dead, ident)
 		}
 	}
 }
--- test/640_proxy_restart_reattaches_test.sh
+++ test/640_proxy_restart_reattaches_test.sh
@@ -4,6 +4,7 @@
 
 NAME=seetwo.weave.local
 
+N=50
 start_suite "Proxy restart reattaches networking to containers"
 
 weave_on $HOST1 launch
@@ -14,5 +15,11 @@ C2=$(container_ip $HOST1 c2)
 proxy docker_on $HOST1 restart --time=1 c2
 assert_raises "proxy exec_on $HOST1 c2 $CHECK_ETHWE_UP"
 assert_dns_record $HOST1 c1 $NAME $C2
+
+# Create and remove a lot of containers; the failure mode is that this
+# takes a long time as it has to wait for the old ones to time out.
+for i in $(seq $N); do
+    proxy docker_on $HOST1 run -e WEAVE_CIDR=net:10.32.4.0/28 --rm -t $SMALL_IMAGE /bin/true
+done
 
 end_suite
